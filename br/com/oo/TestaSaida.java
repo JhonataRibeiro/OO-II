403371f2a35422686f8716adf1c21008e4396c35
= <jhonata.ribeiro.ti@gmail.com>
2016-02-15 22:08:47 -0200

    some changes

--- br/com/oo/TestaSaida.java
+++ br/com/oo/TestaSaida.java
@@ -1,7 +1,19 @@
 package br.com.oo;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 public class TestaSaida {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		OutputStream os = new FileOutputStream("sailda.txt",true);
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
 		
+		bw.write("ok");
+		bw.close();
 	}
 }
---
+++ br/com/oo/TesteIO.java
@@ -0,0 +1,15 @@
+package br.com.oo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class TesteIO {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	}
+
+}
