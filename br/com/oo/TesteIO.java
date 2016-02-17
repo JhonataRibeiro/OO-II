be00540cc63d4935df8137797e862db41b109deb
= <jhonata.ribeiro.ti@gmail.com>
2016-02-16 22:59:17 -0200

    playing with standard input

--- br/com/oo/TesteIO.java
+++ br/com/oo/TesteIO.java
@@ -2,14 +2,17 @@ package br.com.oo;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class TesteIO {
 	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String n;
+		while(( n = in.readLine()) != null);
 	}
 
 }
