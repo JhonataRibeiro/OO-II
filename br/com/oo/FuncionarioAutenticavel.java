52a4e33c53731677c7156f14dd3343f37f0cb7e2
= <jhonata.ribeiro.ti@gmail.com>
2016-02-24 22:26:11 -0300

    just align

--- br/com/oo/FuncionarioAutenticavel.java
+++ br/com/oo/FuncionarioAutenticavel.java
@@ -5,12 +5,10 @@ import br.com.jhonata.oo.modelo.Funcionario;
 public abstract class FuncionarioAutenticavel extends Funcionario{
 	
 	protected int senha;
-
 	
 	public  boolean autentica(int senha){
 		return this.senha == senha;
 	}
-
 
 	@Override
 	public double getBonus() {
